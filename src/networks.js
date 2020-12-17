bf06aea52043dc12657533d9907cf78ceef3b9fd
Michael Rodriguez <southsideprod@hotmail.com>
2020-12-17 13:02:29 -0500

    version bytes

--- src/networks.js
+++ src/networks.js
@@ -28,8 +28,8 @@ module.exports = {
     messagePrefix: '\x19Litecoin Signed Message:\n',
     bech32: 'ltc',
     bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
+      public: 0x019da462,
+      private: 0x019d9cfe
     },
     pubKeyHash: 0x30,
     scriptHash: 0x32,
@@ -39,11 +39,11 @@ module.exports = {
     messagePrefix: '\x19Litecoin Signed Message:\n',
     bech32: 'tltc',
     bip32: {
-      public: 0x043587cf,
-      private: 0x04358394
+      public: 0x0436EF7D,
+      private: 0x0436F6E1
     },
-    pubKeyHash: 0x03,
-    scriptHash: 0x28,
+    pubKeyHash: 0x6F,
+    scriptHash: 0xc4,
     wif: 0xef
   }
 }
